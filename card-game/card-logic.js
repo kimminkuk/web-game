a3f574f138aa53fa837dbe598b630303458fb322
kimminkuk <rlaal567@gmail.com>
2025-05-29 22:57:50 +0900

    이모지를 가져와서, html에 보여주는거까지 성공

--- card-game/card-logic.js
+++ card-game/card-logic.js
@@ -1,12 +1,46 @@
+async function fetchEmojis(count) {
+    try {
+        const response = await fetch('/api/emojis');
+        if (!response.ok) {
+            throw new Error('Function: fetchEmojis, Failed fetchEmojis to fetch emojis');
+        }
+        const emojis = await response.json();
+        // emojis의 총 개수에서, count만큼 랜덤하게 가져오기
+        emojis.sort(() => Math.random() - 0.5); // 랜덤하게 섞기
+        return emojis.slice(0, count);
+    } catch (error) {
+        console.error('Function:fetchEmojis, Error fetchEmojis emojis:', error);
+        return [];
+    }
+}
+
+function displayEmojis(emojis) {
+    const container = document.getElementById('card-make-position');
+    container.innerHTML = ''; // Clear Previous Content
+
+    emojisTwiceCopy = [...emojis, ...emojis]; // 이모티콘을 두 번 복사하여 배열 생성
+    emojisTwiceCopy.sort(() => Math.random() - 0.5); // 랜덤하게 섞기
+    // 이모티콘을 화면에 추가
+    emojisTwiceCopy.forEach(emoji => {
+        const emojiElement = document.createElement('div');
+        emojiElement.textContent = emoji; //이모티콘 추가
+        emojiElement.className = 'emoji-item'; // 스타일 추가 위해서
+        container.appendChild(emojiElement);
+    });
+}
+
 function easy() {
     console.log("Easy level selected");
+    fetchEmojis(8).then(displayEmojis);
 }
 
 function medium() {
     console.log("Medium level selected");
+    fetchEmojis(16).then(displayEmojis);
 }
 
 function hard() {
     console.log("Hard level selected");
+    fetchEmojis(32).then(displayEmojis);
 }
 
--- card-game/proxy-server.mjs
+++ card-game/proxy-server.mjs
@@ -12,6 +12,9 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
+// 정적 파일 제공 설정
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
   // res.send('Proxy server is running. Use /api/emojis to fetch emojis.');
   res.sendFile(path.join(__dirname, 'index.html'));
@@ -27,8 +30,10 @@ app.get('/api/emojis', async (req, res) => {
     
     const data = await response.json(); // JSON 데이터를 먼저 파싱
     const emojis = data.map(emoji => emoji.character); // 이모지 문자만 추출
-    // console.log(emojis); // 이모지 문자 출력
+    // 전체 emojis 전부 가져오기
+    // const emojisAll = data.map(emoji => emoji); // 전체 이모지 객체 배열 가져오기
     res.json(emojis); // 클라이언트에 이모지 문자 배열을 JSON 형태로 응답
+    //res.json(JSON.stringify(emojisAll)); // 전체 이모지 객체 배열을 JSON 형태로 응답
   } catch (error) {
     res.status(500).send('[2] Error fetching emojis');
   }
